f401aac902e742686b8501d98b910f7fa681e713
Anastasya <aamokeeva@yandex.ru>
2025-05-09 09:14:51 +0300

    Простые запросы на получение данных

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::controller(ProductController::class)
+    ->prefix('products')
+    ->group(function () {
+        Route::get('', 'index')->name('products.index');
+        Route::get('{product}', 'show')->name('products.show');
+    });
