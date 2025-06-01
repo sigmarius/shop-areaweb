53e54be37af48d7e05ca8c60022a7556f7ba6b2d
Anastasya <aamokeeva@yandex.ru>
2025-06-01 15:30:30 +0300

    Разделение маршрутов текущего пользователя, и остальных пользователей по группам

---
+++ routes/api/groups/v1/current_user.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Http\Controllers\Api\v1\LoginController;
+use App\Http\Controllers\Api\v1\RegisterController;
+use App\Http\Controllers\Api\v1\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::group(['prefix' => '/user', 'as' => 'user.'], function () {
+    Route::post('/register', RegisterController::class)
+        ->name('register');
+
+    Route::post('/login', LoginController::class)
+        ->name('login');
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::controller(UserController::class)->group(function () {
+            Route::get('/', 'profile')
+                ->name('profile');
+
+            Route::patch('/', 'updateProfile')
+                ->name('update');
+
+            Route::post('avatar', 'uploadAvatar')
+                ->name('avatar.upload');
+        });
+    });
+});
